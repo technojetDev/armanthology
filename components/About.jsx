62a4fed7563021dd8599e4d5ecbd84cbfa2ed20e
Arllan <60104295+MrSyntax31@users.noreply.github.com>
2024-02-27 12:45:57 +0800

    Update About.jsx
    Update About Info

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../public/assets/about.jpg';
+import AboutImg from '../public/assets/IMG_2939_5R_Arllan.jpg';
 
 const About = () => {
   return (
@@ -13,22 +13,15 @@ const About = () => {
           </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+           I'm currently employed at Datawords 24/7 PH, a Multi-cultural and International company, as a Digital Specialist I at their Makati City office here in Metro Manila.
           </p>
           <p className='py-2 text-gray-600'>
-            I started web developement in 2013 managing multiple e-commerce
-            websites on CMS platforms such as WordPress, BigCommerce, and
-            Shopify. I have experience working directly with clients and taking
-            mock wireframes all the way to deployed applications. In my spare
-            time I run Code Commerce, a Youtube channel where I teach web
-            developement and various front-end technologies.
+            As a JR. Digital Specialist, my duties and responsibilities include integrating updates via a content management system (CMS). 
+            It involves investigation of the issues on the websites, activities in the JSONs and scripts, documentation, and reports of bugs, errors, and progress in the tickets. 
+            Most of my integrations involve investigations, readings, and localization of different contents in the respective locales of the brands that the company manages.
+            I'm enjoying my time as a new member of this expanding company, where I participate in team projects, and communicate with clients and project managers all over the world.
+            <br> </br>
+            "Software undergoes beta testing shortly before it’s released. Beta is Latin for still doesn’t work." - Unknown
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
