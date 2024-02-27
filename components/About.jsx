f19726b7bf7c151f839eca58f769d0ff018c5f80
Arllan <60104295+MrSyntax31@users.noreply.github.com>
2024-02-27 12:51:09 +0800

    Update About.jsx
    Revert

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../public/assets/IMG_2939_5R_Arllan.jpg';
+import AboutImg from '../public/assets/about.jpg';
 
 const About = () => {
   return (
@@ -13,12 +13,22 @@ const About = () => {
           </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
-           Test
+            I specialize in building mobile responsive front-end UI applications
+            that connect with API’s and other backend technologies. I’m
+            passionate about learning new technologies and understand there is
+            more than one way to accomplish a task. Though I am most proficient
+            in building front-end applications using HTML, CSS, Javascript, and
+            React, I am a quick learner and can pick up new tech stacks as
+            needed. I believe that being a great developer is not using one
+            specific language, but choosing the best tool for the job.
           </p>
           <p className='py-2 text-gray-600'>
-           Test
-            <br> </br>
-            Software undergoes beta testing shortly before it’s released. Beta is Latin for still doesn’t work. - Unknown
+            I started web developement in 2013 managing multiple e-commerce
+            websites on CMS platforms such as WordPress, BigCommerce, and
+            Shopify. I have experience working directly with clients and taking
+            mock wireframes all the way to deployed applications. In my spare
+            time I run Code Commerce, a Youtube channel where I teach web
+            developement and various front-end technologies.
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
