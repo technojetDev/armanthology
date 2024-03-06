659180c7ba287febb4eb8cc3888bc734420a4b9f
Arllan <60104295+MrSyntax31@users.noreply.github.com>
2024-03-06 17:59:25 +0800

    Update Skills.jsx
    Cange attrtibute

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -103,7 +103,7 @@ const Skills = () => {
          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
                 <div className="m-auto">
-                    <image src="{Html}" width="64px" height="64px" alt="/" />
+                    <Image src="{Html}" width="64px" height="64px" alt="/" />
                 </div>
                 <div className="flex flex-col items-center justify-center">
                     <h3>HTML</h3>
